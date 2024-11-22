e1075c294e215d50103370e1c6da9ce1d6230b42
simonlee2216 <74085374+simonlee2216@users.noreply.github.com>
2024-11-23 00:12:56 +0900

    Create aws_lambda_function.py

---
+++ aws_lambda_function.py
@@ -0,0 +1,34 @@
+import boto3
+import pandas as pd
+from io import StringIO
+import random
+import datetime
+
+BUCKET_NAME = 'your-bucket-name'
+FILE_KEY = 'data.csv'
+
+def lambda_handler(event, context):
+    # Generate random data
+    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    new_value = random.randint(0, 100)
+    
+    # Create or update the CSV data
+    try:
+        s3 = boto3.client('s3')
+        csv_obj = s3.get_object(Bucket=BUCKET_NAME, Key=FILE_KEY)
+        df = pd.read_csv(csv_obj['Body'])
+    except:
+        # If the file doesn't exist, create a new dataframe
+        df = pd.DataFrame(columns=['Time', 'Value'])
+    
+    df = pd.concat([df, pd.DataFrame({'Time': [time_now], 'Value': [new_value]})], ignore_index=True)
+    
+    # Save updated CSV to S3
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3.put_object(Bucket=BUCKET_NAME, Key=FILE_KEY, Body=csv_buffer.getvalue())
+    
+    return {
+        'statusCode': 200,
+        'body': 'Data updated successfully!'
+    }
